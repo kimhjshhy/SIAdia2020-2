050708614c8d07578febde76b110abf97fae6621
kimhjshhy <73907538+kimhjshhy@users.noreply.github.com>
2020-12-20 03:38:58 +0900

    Update sketch.js
    수정수정 700*700

--- sketch.js
+++ sketch.js
@@ -1,36 +1,36 @@
 var slider1, slider2, slider3;
 let bugs = [];
 
-function setup() { 
-  createCanvas(400, 400);
-	slider1 = createSlider(0, 255, 0);
-    slider1.position(10, 10);
+function setup() {
+  createCanvas(700, 700);
+  slider1 = createSlider(0, 255, 0);
+  slider1.position(10, 10);
 
-	slider2 = createSlider(0, 255, 0);
-    slider2.position(10, 30);
+  slider2 = createSlider(0, 255, 0);
+  slider2.position(10, 30);
 
-	slider3 = createSlider(0, 255, 0);
-    slider3.position(10, 50);
-	fill('white');
-  
+  slider3 = createSlider(0, 255, 0);
+  slider3.position(10, 50);
+  fill('white');
+
   for (let i = 0; i < 50; i++) {
     bugs.push(new Jitter());
-   }
- }
+  }
+}
 
-function draw() { 
-	var r = slider1.value();
-	var g = slider2.value();
-	var b = slider3.value();
-	background(r, g, b);
-    fill(b, r, g, 200);
-	text('R: '+r,150,25);
-	text('G: '+g,150,45);
-	text('B: '+b,150,65);
-    for (let i = 0; i < bugs.length; i++) {
+function draw() {
+  var r = slider1.value();
+  var g = slider2.value();
+  var b = slider3.value();
+  background(r, g, b);
+  fill(b, r, g, 200);
+  text('R: ' + r, 150, 25);
+  text('G: ' + g, 150, 45);
+  text('B: ' + b, 150, 65);
+  for (let i = 0; i < bugs.length; i++) {
     bugs[i].move();
     bugs[i].display();
-    }
+  }
 }
 
 class Jitter {
